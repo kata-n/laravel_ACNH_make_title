1812424e60415d6fcb93063d6409facc20b1be5b
kata-n <kata.kawa813@gmail.com>
2020-07-02 22:27:57 +0900

    つぶやきリンク修正

--- app/Http/Controllers/TittleMakeController.php
+++ app/Http/Controllers/TittleMakeController.php
@@ -41,7 +41,7 @@ class TittleMakeController extends Controller
 
     //クエリ設定
     $params = array(
-      "status" => $title_word."    詳しくみる：https://application-anch-tittlemaking.shikatana.com"."/title_card/".$maked_data['id'],
+      "status" => $title_word."    詳しくみる：https://application-anch-tittlemaking.shikatana.com"."/title_card/".$maked_data,
       "trim_user" => false,
     );
 
