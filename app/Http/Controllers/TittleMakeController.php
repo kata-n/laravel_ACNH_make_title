49bd558694f8296969a46b56f2ea6c8e706be603
kata-n <kata.kawa813@gmail.com>
2020-07-12 19:32:57 +0900

    scss追加

--- app/Http/Controllers/TittleMakeController.php
+++ app/Http/Controllers/TittleMakeController.php
@@ -40,7 +40,7 @@ class TittleMakeController extends Controller
 
     //クエリ設定
     $params = array(
-      "status" => "今日の肩書き：".$title_word."#どうぶつの森 詳しくみる：https://application-anch-tittlemaking.shikatana.com"."/title_cards/".$maked_data,
+      "status" => "今日の肩書き：".$title_word." #どうぶつの森 詳しくみる：https://application-anch-tittlemaking.shikatana.com"."/title_cards/".$maked_data,
       "trim_user" => false,
     );
 
